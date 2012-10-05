d44e925594abb5dd2cd02596589106599bdf2ebb
Nicholas Sandberg <nss118@gmail.com>
2012-10-04 19:54:43 -0700

    formatted

---
+++ week1/Hw1-7.rb
@@ -0,0 +1,19 @@
+class CartesianProduct
+    include Enumerable
+    # Your code here
+end
+
+
+
+
+#TEST-----------------------------------------------------------------------------------
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# Nothing printed since Cartesian product of anything with an empty collection is empty
